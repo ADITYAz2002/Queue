fc161d186f8738e50d9ce23a316483212b4d13ff
Aditya Chaturvedi <aditya02032002@yahoo.in>
2023-09-10 10:12:06 +0530

    A simple array implementation of queue DS

---
+++ SimpleArrayImplementation.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct Myqueue
+{
+    int size, cap;
+    int *arr;
+
+    Myqueue(int c)
+    {
+        size = 0;
+        cap = c;
+        arr = new int[cap];
+    }
+
+    void enque(int x)
+    {
+        if(isFull())
+            return;
+        arr[size] = x;
+        size++;
+    }
+
+    void deque()
+    {
+        if(isEmpty())
+            return;
+        
+        for(int i = 0; i < size - 1; i++)
+            arr[i] = arr[i+1];
+        
+        size--;
+    }
+
+    int getfront()
+    {
+        if(isEmpty())
+            return -1;
+
+        return 0;
+    }
+
+    int getRear()
+    {
+        if(isEmpty())
+            return -1;
+
+        return size-1;
+    }
+
+    bool isFull()
+    {
+        return (size == cap);
+    }
+
+    bool isEmpty()
+    {
+        return (size == 0);
+    }
+};
